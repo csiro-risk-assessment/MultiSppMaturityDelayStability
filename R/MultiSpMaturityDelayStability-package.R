b4a79003232246443e7854cada94d980ccc20603
ghosack <geoff.hosack@csiro.au>
2024-11-07 16:40:10 +1100

    initial commit for package

---
+++ R/MultiSpMaturityDelayStability-package.R
@@ -0,0 +1,12 @@
+#' Stability Analysis for Multispecies Model with Delayed Maturity
+#'
+#' Numerical and graphical analyses are provided to reproduce the numerical
+#' demonstration of the local stability analysis for three species of competing
+#' mosquito species with delayed maturity as supplementary material for the
+#' article Stability of difference equations with interspecific density
+#' dependence, competition, and maturation delays.
+#'
+#' @keywords internal
+"_PACKAGE"
+
+NULL
